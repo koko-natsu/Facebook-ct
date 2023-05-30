3aa93ec5bd41f2b51c71ff6a2c9651122777e288
yuta higa <h.yuta2314@gmail.com>
2023-05-31 06:33:57 +0900

    Add friendship to UserResource.

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Friend;
+use App\Http\Resources\Friend as FriendResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class User extends JsonResource
@@ -20,6 +22,9 @@ class User extends JsonResource
                 'user_id' => $this->id,
                 'attributes' => [
                     'name' => $this->name,
+                    'friendship' => [
+                        'attributes' => new FriendResource(Friend::friendship($this->id)) 
+                    ]
                 ],
             ],
             'links' => [
