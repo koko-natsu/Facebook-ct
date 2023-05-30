5d896fc691c98a5bdda892c831918d209b461bfd
yuta higa <h.yuta2314@gmail.com>
2023-05-31 06:50:26 +0900

    Crerate static method: friendship

--- app/Models/Friend.php
+++ app/Models/Friend.php
@@ -5,11 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Friend extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'friend_id',
+        'user_id',
+        'confirmed_at',
+        'status',
     ];
+
+    protected $dates = ['confirmed_at'];
+
+    public static function friendship(string $userId)
+    {
+        return (new static)
+            ->where(function ($query) use($userId) {
+                return $query->where('user_id', auth()->id())
+                    ->where('friend_id', $userId);
+            })
+            ->orWhere(function ($query) use($userId) {
+                return $query->where('friend_id', auth()->id())
+                    ->where('user_id', $userId);
+            })
+            ->first();
+    }
 }
