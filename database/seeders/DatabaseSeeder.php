26c0bee3b1c7221cdff1abdd5ff3368570bc59ce
yuta higa <h.yuta2314@gmail.com>
2023-05-31 06:42:08 +0900

    Add dammy User data.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PostSeeder::class,
         ]);
+
+        User::factory(5)->create();
     }
 }
