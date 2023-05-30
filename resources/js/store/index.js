7fbb5dfd9d481fecc362f2ce09b99757c28c5c48
yuta higa <h.yuta2314@gmail.com>
2023-05-31 06:45:50 +0900

    Create Profile vuex.

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,10 +1,14 @@
 import { createStore } from 'vuex';
 import User from "@/store/modules/User";
 import Title from './modules/Title';
+import Profile from './modules/Profile';
 
 export default createStore({
     modules: {
         User,
         Title,
+        Profile,
+        
+
     }
 });
---
+++ resources/js/store/modules/Profile.js
@@ -0,0 +1,46 @@
+const state = {
+    user: {
+        data: {
+            user_id: '',
+            attributes: {
+                name: ''
+            }
+        }
+    },
+    userStatus: '',
+};
+
+const getters = {
+    user (state) {
+        return state.user;
+    }
+};
+
+const actions = {
+    fetchUser({commit, state}, userId) {
+        commit('setUserStatus', 'loading');
+
+        axios.get('/api/users/' + userId)
+            .then(res => {
+                commit('setUser', res.data);
+                commit('setUserStatus', 'success');
+            })
+            .catch(error => {
+                commit('setUserStatus', 'error');
+            })
+
+    }
+};
+
+const mutations = {
+    setUser(state, user) {
+        state.user = user;
+    },
+    setUserStatus(state, status) {
+        state.userStatus = status;
+    }
+};
+
+export default {
+    state, getters, actions, mutations
+}
