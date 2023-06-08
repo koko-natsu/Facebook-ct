fc3ca1062ff12172bb3c0027b0b208b3d21feec0
yuta higa <h.yuta2314@gmail.com>
2023-06-08 19:43:12 +0900

    changed vue-router setting: users path.

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,20 +2,28 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import NewsFeed from "@/views/NewsFeed.vue";
 import UserShow from "@/views/Users/Show.vue";
 
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: NewsFeed,
+        meta: {
+            title: 'News Feed',
+        }
+    },
+    {
+        path: '/users/:userId(\\d+)?',
+        name: 'user.show',
+        component: UserShow,
+        meta: {
+            title: 'Profile',
+        }
+    },
+];
+
+
 export default createRouter({
     history: createWebHashHistory(),
-    routes: [
-        {
-            path: '/', name: 'home', component: NewsFeed,
-            meta: {
-                title: 'News Feed',
-            }
-        },
-        {
-            path: '/users/:userId', name: 'user.show', component: UserShow,
-            meta: {
-                title: 'Profile',
-            }
-        },
-    ]
+    routes,
 })
