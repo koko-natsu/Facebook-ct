0f04a7c6fc62ed1c2a6056917d4dae970e18da78
yuta higa <h.yuta2314@gmail.com>
2023-06-09 19:07:30 +0900

    Added path of Like controller.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostLikeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserPostController;
 use App\Http\Controllers\AuthUserController;
@@ -16,6 +17,7 @@ Route::middleware('auth:api')->group(function () {
     Route::apiResources([
        'users' => UserController::class,
        'posts' => PostController::class,
+       'posts/{post}/like' => PostLikeController::class,
        'users/{user}/posts' => UserPostController::class,
        '/friend-request' => FriendRequestController::class,
        '/friend-request-response' => FriendRequestResponseController::class,
