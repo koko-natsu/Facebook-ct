55c3425eb5545a57c4f61f8a9fbed3000d57ff47
yuta higa <h.yuta2314@gmail.com>
2023-06-09 19:08:39 +0900

    Added post method to vuex.

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,13 +2,13 @@ import { createStore } from 'vuex';
 import User from "@/store/modules/User";
 import Title from './modules/Title';
 import Profile from './modules/Profile';
+import Post from './modules/Post';
 
 export default createStore({
     modules: {
         User,
         Title,
         Profile,
-        
-
+        Post,
     }
 });
---
+++ resources/js/store/modules/Post.js
@@ -0,0 +1,77 @@
+const state = {
+    newsPosts: {},
+    newsPostsStatus: {},
+    postMessage: '',
+};
+
+const getters = {
+    newsPosts: state => {
+        return state.newsPosts;
+    },
+    newsPostsStatus: state => {
+        return {
+            newsStatus: state.newsPostsStatus
+        }
+    },
+    postMessage: state => {
+        return state.postMessage;
+    }
+};
+
+const actions = {
+    fetchNewsPosts({commit, state}) {
+        commit('setPostsStatus', 'loading');
+
+        axios.get('/api/posts')
+        .then(res => {
+            commit('setPosts', res.data);
+            commit('setPostsStatus', 'success');
+        })
+        .catch(error => {
+            commit('setPostsStatus', 'error');
+        })
+    },
+
+    postMessage({commit, state}) {
+        commit('setPostsStatus', 'loading');
+
+        axios.post('/api/posts', { body: state.postMessage })
+        .then(res => {
+            commit('pushPost', res.data);
+            commit('updateMessage', '');
+            commit('setPostsStatus', 'success');
+        })
+        .catch(error => {
+        })
+    },
+    likePost({commit, state}, data) {
+        axios.post('/api/posts/' + data.postId + '/like')
+            .then(res => {
+                commit('pushLikes', { likes: res.data, postKey: data.postKey });
+            })
+            .catch(error => {
+            })
+    }
+};
+
+const mutations = {
+    setPosts(state, posts) {
+        state.newsPosts = posts;
+    },
+    setPostsStatus(state, status) {
+        state.newsPostsStatus = status;
+    },
+    updateMessage(state, message) {
+        state.postMessage = message;
+    },
+    pushPost(state, post) {
+        state.newsPosts.data.unshift(post);
+    },
+    pushLikes(state, data) {
+        state.newsPosts.data[data.postKey].data.attributes.likes = data.likes;
+    }
+};
+
+export default {
+    state, getters, actions, mutations
+}
